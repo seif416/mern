8ca17680812e9b31c8d6b28ae84edf0c8e1b97fa
Your Name <you@example.com>
2024-06-23 01:56:20 +0200

    first commit

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ mongoose.connect(MONGODB_URI)
     console.log('Connected to MongoDB');
     app.use(express.json());
     app.use('/api', routes);
-    app.listen(PORT, () => console.log('https://mern-med11.onrender.com'));
+    app.listen(PORT, () => console.log('https://mern-med12.onrender.com'));
   })
   .catch(err => console.error('Error connecting to MongoDB:', err));
